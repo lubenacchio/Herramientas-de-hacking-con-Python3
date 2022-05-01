93d728bf2e4aa1d76a59a9bf21e8386ecd9885c4
lubenacchio <lu.benacchio@duocuc.cl>
2022-05-01 02:26:39 -0400

    portScanner v1.2

--- port-scanner.py
+++ port-scanner.py
@@ -2,13 +2,13 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "100.00.0.0"  # IPv4 adress of the target
-port = 445  # port tarjet
+host = input(" -$- Ingrese Host a Escanear : ")
+port = int(input(" -$- Ingrese Puerto a Escanear : "))
 
 
 def portScanner(port):
     if sock.connect_ex((host, port)):
-        print('Port %d is closed' % (port))
+        print('Port %d is open' % (port))
     else:
         print('port %d is closed' % (port))
 
